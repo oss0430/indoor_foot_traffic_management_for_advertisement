c1cffd7dc24b0aaa5c7854a0ff60ea5baaab8d4d
Donghoon Lee <oss002@ajou.ac.kr>
2022-11-04 13:57:22 +0900

    device blueprint laid

---
+++ src/billboardApplication.py
@@ -0,0 +1,26 @@
+import time
+
+class Camera():
+
+    ## A camera class for smart billbaords camera application
+
+    def __init__(self) -> None:
+        return
+
+    def _takePicture(self):
+        return
+
+    def _savePicture(self):
+        return
+
+    def runByTimer(self) -> None:
+        return
+
+class Screen():
+    def __init__(self) -> None:
+        return
+
+    def showOnScreen(self) -> None:
+        return
+
+        
