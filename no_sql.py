a62025fd9a866e1b7da553795fc036ca01d35b1d
Donghoon Lee <oss002@ajou.ac.kr>
2022-12-06 18:07:55 +0900

    Blue print laid for Bilboard

---
+++ no_sql.py
@@ -0,0 +1,51 @@
+billboard_image = [
+    {
+        "billboard_image_id" : 1,
+        "promoting_item_ids": [12,14],
+        "up_date" :"2022-12-03 14:07:05",
+        "down_date" : "2022-12-03 14:07:05"
+    }
+]
+
+
+face_recognition = [
+    {
+        "billboard_image_id" : 1,
+        "face_count" : 3,
+        "time_stamp" : "2022-12-03 14:07:05"
+    },
+    {
+        "billboard_image_id" : 1,
+        "face_count" : 3,
+        "time_stamp" : "2022-12-03 14:07:20"
+    }
+]
+
+item = [
+    {
+        "item_id" : 10,
+        "item_location_x" : 0.531,
+        "item_location_y" : 0.412,
+        "item_price" : 10000   
+    }
+]
+
+
+location = [
+    {
+        "user_id" : 1,
+        "x" : 0.12352213,
+        "y" : 0.35221234,
+        "time_stamp" : "2022-12-03 14:07:05"
+    }
+]
+
+
+sales = [
+    {
+        "item_id" : 12,
+        "user_id" : 14,
+        "sales"   : 2,
+        "time_stamp" : "2022-12-03 14:07:05"
+   }
+]
--- src/billboardApplication.py
+++ src/billboardApplication.py
@@ -1,5 +1,18 @@
 import time
 
+class MovementSensor():
+
+    def __init__(self) -> None:
+        return
+
+    def detect_movement(self):
+        ## In different Thread keep detecting untill it spots movement
+
+        is_movement = True
+        
+        return is_movement
+
+
 class Camera():
 
     ## A camera class for smart billbaords camera application
@@ -23,4 +36,36 @@ class Screen():
     def showOnScreen(self) -> None:
         return
 
-        
+class AWSCloud():
+    def __init__(self
+    ) -> None:
+
+        return
+
+class Bilboard():
+
+    def __init__(
+        self,
+        camera : Camera,
+        screen : Screen,
+        aws_cloud : AWSCloud,
+        movement_sensor : MovementSensor
+    ):
+        self.camera = camera
+        self.screen = screen
+        self.aws_cloud = aws_cloud
+        self.movement_sensor = movement_sensor
+
+    def _is_movement(self):
+        return self.movement_sensor()
+
+    def run_test(
+        self
+    ):
+        ## Shows Test Billboard image through screen
+        ## upload face recognition result to aws
+        ## Along with Billboard image id (for AB TESTING)
+
+        self.movement_sensor.detect_movement()
+
+        None
