44a02f1891e6edbd9cd53ff80011106bf423b179
shear99 <dancheong99@gmail.com>
2022-12-14 14:13:52 +0900

    search_user_local_data 수정

--- src/get_data_from_AWS.py
+++ src/get_data_from_AWS.py
@@ -13,12 +13,14 @@ class Get_data():
     def get_user_local_data_in_dynamoDB(
         self,
         user_id,
-        time
+        time1,
+        time2
     ):
         host = self.aws_url + '/search_user_local_data'
         data = {
             "user_id" : user_id,
-            "time" : time
+            "time1" : time1,
+            "time2" : time2
         }
         response = requests.post(host, data=data, headers=None)
         return_data = json.loads(response.content)
@@ -61,26 +63,30 @@ class Get_data():
 def main():
     get_data = Get_data()
     
-    # user_data = get_data.get_user_local_data_in_dynamoDB(11143,103834)
-    # print(user_data)
+    # id = 11143, sample_time 범위 내의 user local 데이터 가져오기
+    sample_time =  [int(103834),int(104353)]
+    user_data = get_data.get_user_local_data_in_dynamoDB(11143,sample_time[0],sample_time[1])
+    print(user_data)
     
-    # with open("aws_get_data/local_data.json", "w") as outfile:
-    #     json.dump(user_data, outfile, indent=4)
+    with open("aws_get_data/local_data.json", "w") as outfile:
+        json.dump(user_data, outfile, indent=4)
         
-    # #product_id = 6463인 항목 가져오기
-    # product_data = get_data.get_product_data(6463)
-    # print(product_data)
+    # product_id = 6463인 항목 가져오기
+    product_data = get_data.get_product_data(6463)
+    print(product_data)
     
-    # with open("aws_get_data/product_data.json", "w") as outfile:
-    #     json.dump(product_data, outfile, indent=9)
+    with open("aws_get_data/product_data.json", "w") as outfile:
+        json.dump(product_data, outfile, indent=9)
 
-    # product_data = get_data.get_point_of_sales_data(11343)
-    # print(product_data)
+    # product_id = 11343인 상품의 판매기록 가져오기 (point_of_sales)
+    product_data = get_data.get_point_of_sales_data(11343)
+    print(product_data)
     
-    # with open("aws_get_data/point_of_sales_data.json", "w") as outfile:
-    #     json.dump(product_data, outfile, indent=6)         
+    with open("aws_get_data/point_of_sales_data.json", "w") as outfile:
+        json.dump(product_data, outfile, indent=6)         
 
-    #
+    
+    # sample_time 범위 내의 Face_count 데이터 가져오기
     sample_time = [20221208014953,20221208020104]
     face_count_data = get_data.get_face_count_data(sample_time[0], sample_time[1])
     print(face_count_data)
--- src/lambdas/search_user_local_data.py
+++ src/lambdas/search_user_local_data.py
@@ -35,3 +35,45 @@ def lambda_handler(event, context):
         'statusCode' : 200,
         'body' : json.dumps(response)
     }
+    
+    '''
+    data = event.get('body')
+    result = base64.b64decode(data).decode('utf-8')
+    print(list)
+    print(result)
+    
+    newlist = result.split('&')
+    items = {
+        'user_id' : newlist[0][8:],        # primary key
+        'time' : newlist[1][5:]
+    }
+    print(items)
+    
+    dynamodb = boto3.resource('dynamodb')
+    table_name = 'local_data'
+    
+    client = boto3.client('dynamodb')
+    response = client.batch_get_item(
+        RequestItems = {
+            table_name : {
+                'Keys' : [
+                    {
+                        'user_id' : {
+                            'N' : items['user_id'],
+                        },
+                        'time' : {
+                            'N' : items['time'],
+                        },
+                    }
+                ]
+            }
+        }
+    )
+    result = response['Responses'].get(table_name)
+    print(result)
+
+    return {
+        'statusCode' : 200,
+        'body' : json.dumps(result)
+    }
+    '''
